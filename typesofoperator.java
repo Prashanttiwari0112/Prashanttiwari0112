9f01b08f23df001c426c7f30db338e57a8c81860
PRASHANT TIWARI <prashanttiwari0705@gmail.com>
2024-12-18 20:06:55 +0530

    java
    java operator

---
+++ typesofoperator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class typesofoperator {
+    public static void main(String[] args) {
+       //#uniry operators
+       //++a(print increment) firt a ki value badhegi fir use hogi exa
+    //    int a = 10;
+    //    int b = ++a;
+    //    System.out.println(a);
+    //    System.out.println(b);
+       // a++(post increment) pehele use hoga phhir badhega 
+    //    int a = 10;
+    //    int b = a++;
+    //    System.out.println(a);
+    //    System.out.println(b);
+    //# Relational operator  
+    // == for equal, !=for not equal
+    //# logical operator 
+    // && use for AND ex (true AND true = true)
+    // System.out.print((3<4) && (6>4));
+    // || for OR ex (false || false = false)
+    // System.out.print((3<4) || (6>4));
+    // ! = NOT 
+    System.out.print(!(1>5));
+
+    }
+}
